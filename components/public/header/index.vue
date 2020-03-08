447e2a777a923016ca6cfae62b31d6a0c4d729ad
wangwj <wangwj@ui-tech.com.cn>
2020-03-08 15:43:54 +0800

    文件删除

--- components/public/header/index.vue
+++ components/public/header/index.vue
@@ -1,23 +1,48 @@
 <template>
   <div class="m-header">
-    <el-row>
-      <el-col>
-        <top-bar />
+    <el-row :gutter="0">
+      <el-col :span="5">
+        <div class="m-geo">
+          <i class="el-icon-location" />
+          <nuxt-link class="changeCity" to="/changeCity">切换城市</nuxt-link>[太原 北京 鞍山]
+        </div>
+      </el-col>
+      <el-col :span="5">
+        <div class="m-user">
+          <template v-if="user">
+            欢迎您，
+            <span>wwj</span>
+            <nuxt-link to="/exit">退出</nuxt-link>
+          </template>
+          <template v-else>
+            <nuxt-link to="/login" class="login">立即登陆</nuxt-link>
+            <nuxt-link to="/register" class="register m-l-10">注册</nuxt-link>
+          </template>
+        </div>
+      </el-col>
+      <el-col :span="14">
+        <div class="m-nav">
+          <ul class="nav">
+            <li>我的美团</li>
+            <li>手机APP</li>
+            <li>商家中心</li>
+            <li>网站导航</li>
+          </ul>
+        </div>
       </el-col>
     </el-row>
     <el-row>
-      <el-col>
-        <!-- <search-bar /> -->
-      </el-col>
+      <el-col></el-col>
     </el-row>
   </div>
 </template>
 
 <script>
-import TopBar from "./topBar";
 export default {
-  components: {
-    TopBar
+  data() {
+    return {
+      user: false
+    };
   }
 };
 </script>
