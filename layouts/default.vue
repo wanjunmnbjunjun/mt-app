d21ca842b15534be23fb7235bdc2c4b14a30432e
wangwj <wangwj@ui-tech.com.cn>
2020-03-20 14:15:36 +0800

    登录注册静态页面

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,16 +4,20 @@
       <my-header></my-header>
     </el-header>
     <el-main>
-      <nuxt />
+      <!-- <nuxt /> -->
     </el-main>
-    <!-- <el-footer>Footer</el-footer> -->
+    <el-footer>
+      <my-footer />
+    </el-footer>
   </el-container>
 </template>
 <script>
 import MyHeader from "@/components/public/header/index";
+import MyFooter from "@/components/public/footer/index"
 export default {
   components: {
-    MyHeader
+    MyHeader,
+    MyFooter
   }
 };
 </script>
